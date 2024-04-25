372f564ec9120daccabdacd94eedbccaa58d66cb
vzanbr <gabriecvr123@gmail.com>
2024-04-25 16:22:39 -0300

    Fix : Corrigindo o gerador agencia para utilizar um prefixo!

--- src/main/java/GerarAgencia.java
+++ src/main/java/GerarAgencia.java
@@ -1,12 +1,18 @@
 import java.util.Random;
 
 public class GerarAgencia {
+
+    private static final String [] PREFIXO_AGENCIA =
+            {"562-", "562-", "562-"};
     public static String gerarAgencia() {
         Random random = new Random();
 
-        StringBuilder gerarAgencia = new StringBuilder();
-        for (int i = 0; i < 4; i++) {
-            gerarAgencia.append(random.nextInt(7));
+        int indicePrefixo = random.nextInt(PREFIXO_AGENCIA.length);
+        String prefixo = PREFIXO_AGENCIA[indicePrefixo];
+
+        StringBuilder gerarAgencia = new StringBuilder(prefixo);
+        for (int i = 0; i < 1; i++) {
+            gerarAgencia.append(random.nextInt(7 ));
         }
         return gerarAgencia.toString();
     }
